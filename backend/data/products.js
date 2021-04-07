47e9a315f9b8eea243e1181e35222eb5bf8d9ce1
Rishabh Agarwal <55267423+coolspencer@users.noreply.github.com>
2021-04-07 13:33:09 +0530

    Add files via upload

---
+++ backend/data/products.js
@@ -0,0 +1,52 @@
+const products = [
+  {
+    name: "Noodles",
+    image: "/images/01.jpg",
+    price: 3.99,
+    countInStock: 15,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    name: "PanCakes",
+    image: "/images/02.jpg",
+    price: 5.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    name: "Fish",
+    image: "/images/03.jpg",
+    price: 9.59,
+    countInStock: 16,
+    rating: 4.9,
+    numReviews: 12,
+  },
+  {
+    name: "Pasta",
+    image: "/images/04.jpg",
+    price: 7.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    name: "Chicken Burger",
+    image: "/images/05.jpg",
+    price: 6.99,
+    countInStock: 7,
+    rating: 4.5,
+    numReviews: 10,
+  },
+  {
+    name: "Softy",
+    image: "/images/06.jpg",
+    price: 2.99,
+    countInStock: 10,
+    rating: 4,
+    numReviews: 12,
+  },
+];
+
+export default products;
